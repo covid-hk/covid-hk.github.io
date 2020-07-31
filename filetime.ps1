08bf2ce744fd88e7dc062b86f5b2cfc81becdb57
nielist <nielist@gmail.com>
2020-07-31 18:50:48 +0800

    Add files via upload

---
+++ filetime.ps1
@@ -0,0 +1,50 @@
+# # Run PowerShell as Admin
+# Set-ExecutionPolicy RemoteSigned
+
+$filetimecsvfilename = "filetime.csv"
+$filetimecsvfilepath = Join-Path $PSScriptRoot $filetimecsvfilename
+
+if (![System.IO.File]::Exists($filetimecsvfilepath)) {
+  # filetime.csv init
+  Set-Content -Path $filetimecsvfilepath -Value "file_time,file_name,file_md5" -Force
+  $files = @()
+  $files = Get-ChildItem -Path $PSScriptRoot -Recurse -File -Filter *.csv
+  $files |`
+  Foreach-Object {
+    $filename = $_.Name
+    $filepath = $_.FullName
+    $filetime = $_.LastWriteTime.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss")
+    if (-NOT ($filename -eq $filetimecsvfilename)) {
+      $md5 = (Get-FileHash $filepath -Algorithm MD5).Hash
+      Add-Content -Path $filetimecsvfilepath -Value $filetime","$filename","$md5 -Force
+    }
+  }
+}
+else {
+  # filetime.csv update
+  $file_time = @()
+  $file_name = @()
+  $file_md5 = @()
+  Import-Csv $filetimecsvfilepath |`
+  ForEach-Object {
+    $filename = $_."file_name"
+    $filepath = Join-Path $PSScriptRoot $filename
+    $filetime = $_."file_time"
+    $filemd5 = $_."file_md5"
+    if (-NOT ($filename -eq $filetimecsvfilename)) {
+      $md5 = (Get-FileHash $filepath -Algorithm MD5).Hash
+      if (-NOT ($md5 -eq $_."file_md5")) {
+        $filetime = (Get-Item $filepath).LastWriteTime.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss")
+        $filename = $_."file_name"
+        $filemd5 = $md5
+      }
+      $file_time += $filetime
+      $file_name += $filename
+      $file_md5 += $filemd5
+    }
+  }
+  Set-Content -Path $filetimecsvfilepath -Value "file_time,file_name,file_md5" -Force
+  for ($i = 0; $i -lt $file_md5.Count; $i++) {
+    Add-Content -Path $filetimecsvfilepath -Value "$($file_time[$i]),$($file_name[$i]),$($file_md5[$i])" -Force
+  }
+}
--- webcrawler.cmd
+++ webcrawler.cmd
@@ -4,3 +4,5 @@ curl -o "./enhanced_sur_covid_19_chi.csv" "https://www.chp.gov.hk/files/misc/enh
 REM curl -o "./enhanced_sur_covid_19_eng.csv" "https://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv"
 curl -o "./latest_situation_of_reported_cases_covid_19_chi.csv" "https://www.chp.gov.hk/files/misc/latest_situation_of_reported_cases_covid_19_chi.csv"
 REM curl -o "./latest_situation_of_reported_cases_covid_19_eng.csv" "https://www.chp.gov.hk/files/misc/latest_situation_of_reported_cases_covid_19_eng.csv"
+
+powershell -File "./filetime.ps1"
