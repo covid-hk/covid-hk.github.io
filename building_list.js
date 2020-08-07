290204eefe6cd16d9cca940f6c27d95631569596
nielist <nielist@gmail.com>
2020-08-08 00:07:20 +0800

    Add files via upload

--- building_list.js
+++ building_list.js
@@ -1,6 +1,6 @@
 var deferredPrompt;
 var downloadAppBtn;
-$(window).on('load'/*'beforeinstallprompt'*/, (e) => {
+$(window).on('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
