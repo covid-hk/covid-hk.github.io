fb443ae5c154f8a13a773b38d0148ba7f97ae8b2
nielist <nielist@gmail.com>
2020-08-07 17:15:12 +0800

    Add files via upload

--- building_list.js
+++ building_list.js
@@ -1,3 +1,31 @@
+var deferredPrompt;
+var downloadAppBtn;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  downloadAppBtn = document.querySelector('#downloadAppBtn');
+  downloadAppBtn.style.display = 'block';
+
+  downloadAppBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    downloadAppBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          //console.log('User accepted the A2HS prompt');
+        } else {
+          //console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+
 var building_list = [];
 var building_list_dedup = [];
 var googleapis_maps_hashmap = [];
