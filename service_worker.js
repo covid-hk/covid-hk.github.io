f379027e23b9a37c266670bb087321dc49733fe4
nielist <nielist@gmail.com>
2020-08-07 16:36:03 +0800

    Add files via upload

--- service_worker.js
+++ service_worker.js
@@ -19,7 +19,7 @@
  * https://www.w3.org/TR/service-workers/
 **/
 
-var CACHE_NAME = 'covid-cache-v2.2';
+var CACHE_NAME = 'covid-cache-v2.4';
 var urlsToCache = [
   '/',
   '/index.html',
@@ -100,4 +100,33 @@ self.addEventListener('activate', function(event) {
       );
     })
   );
+});
+
+let deferredPrompt;
+const downloadAppBtn = document.querySelector('#downloadAppBtn');
+downloadAppBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  downloadAppBtn.style.display = 'block';
+
+  downloadAppBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    downloadAppBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          //console.log('User accepted the A2HS prompt');
+        } else {
+          //console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
 });
