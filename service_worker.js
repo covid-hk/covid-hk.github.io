bad09f33393196ef0ae03f84197c5537f492b31d
nielist <nielist@gmail.com>
2020-12-17 12:15:01 +0800

    Add files via upload

--- service_worker.js
+++ service_worker.js
@@ -19,7 +19,7 @@
  * https://www.w3.org/TR/service-workers/
 **/
 
-var CACHE_NAME = 'covid-cache-v2.25';
+var CACHE_NAME = 'covid-cache-v2.26';
 var unixtimestamp = Math.floor(Date.now() / 1000);
 var urlsToCache = [
   '/',
